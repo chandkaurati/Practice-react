39872f482149b11e7eacc58a6f3f54814b06ed5d
chandkaurati <chandkaurati@outlook.com>
2024-09-27 15:03:16 +0530

    learned about componant lyfecycle

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element : <Home/>
-      },
+      }, 
       {
         path: "/about",
         element: <About />,
--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      About
+export default class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Chandkaurati",
+      count: 0,
+      users: [],
+    };
 
-      hello moon
-    </div>
-  )
+
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    this.setState((prevstate) => ({
+      count: prevstate.count + 1,
+    }));
+  }
+
+  getData = () => {
+    console.log(this.state.name);
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentDidUpdate() {
+    console.log("componnant updaated");
+  }
+
+  componentWillUnmount(){
+    console.log("componant has been unmounted")
+  }
+
+  render() {
+    return (
+      <div>
+        <p>hi {this.state.name}</p>
+        <p>count : {this.state.count}</p>
+        <button onClick={this.increment}>incremnt</button>
+      </div>
+    );
+  }
 }
-
-export default About
