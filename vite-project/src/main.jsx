d9bfa4a22517dfd6400e2e76c329a0ef6db7a303
chandkaurati <chandkaurati@outlook.com>
2024-09-28 17:01:33 +0530

    rewise lyfecycle methods

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -30,9 +30,8 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <RouterProvider router={router}>
       <App/>
     </RouterProvider>
-  </StrictMode>,
 )
--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 
 export default class About extends Component {
   constructor(props) {
+    console.log("constructor");
     super(props);
     this.state = {
       name: "Chandkaurati",
       count: 0,
-      users: [],
+      products: [],
+      loading : true,
+      error : ""
     };
-
-
-    this.increment = this.increment.bind(this)
+    this.timerId = null
+    this.increment = this.increment.bind(this);
+    this.getProducts = this.getProducts.bind(this)
   }
 
   increment() {
@@ -19,27 +22,50 @@ export default class About extends Component {
     }));
   }
 
-  getData = () => {
-    console.log(this.state.name);
-  };
-
   componentDidMount() {
-    this.getData();
+    console.log("compoanant mounted");
+    // this.getData();
+  //  this.timerId =  setInterval(() => {
+  //     console.log("hi")
+  //   }, 1000);
+    
+    this.getProducts()
   }
 
   componentDidUpdate() {
     console.log("componnant updaated");
   }
 
-  componentWillUnmount(){
-    console.log("componant has been unmounted")
+  componentWillUnmount() {
+    console.log("componant has been unmounted");
+    clearInterval(this.timerId)
+  }
+
+
+  async getProducts() {
+
+       try {
+         const responce = await fetch("https://dummyjson.com/products/categories")
+         const data = await responce.json()
+         this.setState({
+          products : data,
+          loading: false
+         })
+       } catch (error) {
+        this.setState({
+          error: error?.message,
+          loading : false
+        })
+       }
   }
 
   render() {
     return (
       <div>
+        {console.log("render")}
         <p>hi {this.state.name}</p>
         <p>count : {this.state.count}</p>
+        <p>{this.state.loading ? "loading...." : "products loaded "}</p>
         <button onClick={this.increment}>incremnt</button>
       </div>
     );
